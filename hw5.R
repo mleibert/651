06ec2e5c49185c7789dfa25d80e756e0b3c667ef
mleibert <mike.leibert@gmail.com>
2017-10-16 00:56:42 -0400

    1

--- hw5.R
+++ hw5.R
@@ -23,3 +23,27 @@ cp.ee<-(diag(cp.n)-cp.hat) %*% cp.Y
 as.vector(cp.ee)
 
 boxplot(cp.ee)
+
+
+
+
+
+
+#6.27
+X1=c(7,4,16,3,21,8)
+X2=c(33,41,7,49,5,31)
+Y=c(42,33,75,28,91,55)
+n=length(X1)
+X<-rep(1,n)
+
+X<-as.matrix(data.frame(X,X1,X2));colnames(X)<-NULL
+Y<-as.matrix(data.frame(Y));colnames(Y)<-NULL
+p<-dim(X)[2]
+ 
+b<-solve(t(X)%*%X)%*%t(X)%*%Y;b
+H<-X%*%solve(t(X)%*%X) %*% t(X)
+e<-(diag(n)-H)%*%Y;e
+J<-matrix(1,n,n)
+SSR<-t(b)%*%t(X)%*%Y - (1/n)*t(Y) %*%J  %*% Y
+MSE<- as.numeric((t(e) %*% e) / (n-p))
+MSE*solve(t(X)%*%X)
